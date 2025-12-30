6a4cdb06a9259556091c21e2f94cd38736f9f62b
wayneleon1 <rurangwaleon10@gmail.com>
2025-12-31 01:26:58 +0200

    Setting up backend project with springboot

---
+++ backend/src/test/java/com/example/car_management/CarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
